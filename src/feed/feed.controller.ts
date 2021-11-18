0ced53ebc5bab968fd1d1547831f9bf096907382
Sulaiman <skshanwari830@gmail.com>
2021-11-18 18:13:37 +0500

    update feed module.

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,25 +1,36 @@
-import { Body, Controller, Post, Get } from "@nestjs/common";
-import { Observable } from "rxjs";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { from, Observable } from "rxjs";
+import { UpdateResult } from "typeorm";
 import { FeedService } from "./feed.service";
 import { FeedPost } from "./models/post.Interface";
 
 @Controller('feed')
 export class FeedController {
-    constructor(private feedService: FeedService) {}
-    @Post() 
+    constructor(private feedService: FeedService) { }
+    @Post()
     create(
         @Body()
-        post:FeedPost): Observable<FeedPost> {
+        post: FeedPost): Observable<FeedPost> {
         return this.feedService.createPost(post)
     }
 
-  //   @Get()
-  //   getPosts(): any {
-  //   return this.feedService.getPosts();
-  // }
-
     @Get()
-    getAllPosts(): Observable<FeedPost[]> {
-      return this.feedService.getAllPosts()
+    findAll(): Observable<FeedPost[]> {
+        return this.feedService.findAllPosts();
+    }
+
+    @Put(':id')
+    update(
+        @Param('id') id: number,
+        @Body() feedPost: FeedPost
+    ): Observable<UpdateResult> {
+        return this.feedService.updatePost(id, feedPost)
+    }
+
+    @Delete(':id')
+    delete(
+        @Param('id') id: number
+    ) {
+        return this.feedService.deletePost(id)
     }
 }
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { from, Observable } from 'rxjs';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { FeedPostEntity } from './models/post.entity';
 import { FeedPost } from './models/post.Interface';
 
@@ -9,17 +9,22 @@ import { FeedPost } from './models/post.Interface';
 export class FeedService {
     constructor(
         @InjectRepository(FeedPostEntity)
-        private readonly feedPostRepository:Repository<FeedPostEntity>
-    ) {}
+        private readonly feedPostRepository: Repository<FeedPostEntity>
+    ) { }
 
-    createPost(feedPost: FeedPost):Observable<FeedPost> {
+    createPost(feedPost: FeedPost): Observable<FeedPost> {
         return from(this.feedPostRepository.save(feedPost))
     }
 
-    // getPosts(): Promise<FeedPost[]> {
-    //     return this.feedPostRepository.find();
-    //   }
-    getAllPosts():Observable<FeedPost[]> {
-        return from(this.feedPostRepository.find())
+    findAllPosts(): Observable<FeedPost[]> {
+        return from(this.feedPostRepository.find());
+    }
+
+    updatePost(id: number, feedPost: FeedPost): Observable<UpdateResult> {
+        return from(this.feedPostRepository.update(id, feedPost))
+    }
+
+    deletePost(id: number) {
+        return from(this.feedPostRepository.delete(id))
     }
 }
