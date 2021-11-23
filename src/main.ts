aadcbfff256a650a8a273abaedcb9ee55439e798
sulaiman <skshanwari830@gmail.com>
2021-11-23 12:48:47 -0500

    cors enabled.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3003;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
